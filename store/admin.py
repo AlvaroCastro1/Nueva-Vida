2a6ae34a865e6487740a8ad7198d09cac3b577db
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-10-19 09:20:44 -0600

    Vista de Ordenes

--- store/admin.py
+++ store/admin.py
@@ -8,7 +8,20 @@ class ImagenProductoInline(admin.TabularInline):  # admin.StackedInline
 class IProductoAdmin(admin.ModelAdmin):
     inlines = [ImagenProductoInline]
     
+class OrderItemInline(admin.TabularInline):  # admin.StackedInline
+    model = OrderItem
+    extra = 1 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'date_ordered', 'get_cart_total']
+    inlines = [OrderItemInline]
+
+    list_filter = ['complete']
+    search_fields = ['id', 'customer__name', 'transaction_id']
+
+
 admin.site.register(Customer)
 admin.site.register(Product, IProductoAdmin)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+# admin.site.register(Order)
+# admin.site.register(OrderItem)
+admin.site.register(Order, OrderAdmin)
