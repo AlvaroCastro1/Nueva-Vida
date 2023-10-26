29e870f14c71e0e2dca0cb289f4a60bbee0d337e
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-10-26 08:51:18 -0600

    Donaciones

--- store/models.py
+++ store/models.py
@@ -98,7 +98,7 @@ class ShippingAddress(models.Model):
 		return self.address
 #Registro de una donacion	
 class DonacionRopa(models.Model):
-    donante = models.CharField(max_length=100, help_text="Nombre del donante")
+    Donante = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
     fecha_donacion = models.DateField(help_text="Fecha de la donación")
     descripcion = models.TextField(help_text="Descripción de la donación")
     cantidad = models.PositiveIntegerField(help_text="Cantidad de ropa donada")
@@ -108,4 +108,20 @@ class DonacionRopa(models.Model):
     puntuacion = models.DecimalField(max_digits=3, decimal_places=2, help_text="Puntuación de la donación")
 
     def __str__(self):
-        return f"Donación de {self.donante} el {self.fecha_donacion}"
+        return f"Donación de {self.Donante} el {self.fecha_donacion}"
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=DonacionRopa)
+def actualizar_monedero(sender, instance, **kwargs):
+    # Obtenemos el valor de 'puntuación' de la instancia de DonacionRopa
+    puntuacion_donacion = instance.puntuacion
+
+    # Supongamos que hay una relación ForeignKey o OneToOneField en DonacionRopa
+    # que enlaza a un usuario Customer, llamémosla 'usuario'
+    usuario = instance.Donante
+
+    # Actualizamos el 'Monedero' del usuario en Customer
+    usuario.Monedero += puntuacion_donacion
+    usuario.save()
